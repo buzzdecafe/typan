b790869963e8d9497a8a450a7be872a49091983a
buzzdecafe <m_hur@yahoo.com>
2016-03-27 11:39:41 -0400

    something is working

---
+++ src/gamestate.ml
@@ -0,0 +1,5 @@
+type state = { 
+  firm : string;
+}
+
+let gen firm cash_or_guns = { firm = firm }
--- src/typan.ml
+++ src/typan.ml
@@ -1,6 +1,29 @@
 open Goods
+open Gamestate
+
+let rec get_string valid question =
+  let s = read_line (print_string question) in 
+  if valid s then s 
+  else get_string valid question
+
+let valid_firm s = true (* TODO *)
+
+let valid_cog = function
+  | "1" -> true
+  | "2" -> true
+  | _   -> false
+
+let cash_or_guns_q = "Start with:\n\t1. Cash and a debt; or\n\t2. Five guns and no debt\n? "
 
 
-let main () = 
-  print_string (Goods.string_of_t Goods.Opium)
+let setup () = 
+  let firm = get_string valid_firm "Enter firm name: " in
+  let cash_or_guns = get_string valid_cog cash_or_guns_q in
+  Gamestate.gen firm cash_or_guns
+
+
+let _ = 
+  print_endline ("Let's buy some " ^ (Goods.string_of_t Goods.Opium));
+  let game = setup () in print_endline ("Welcome, " ^ game.firm)
 ;;
+
